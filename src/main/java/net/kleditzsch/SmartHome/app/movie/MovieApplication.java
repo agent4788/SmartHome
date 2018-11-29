9462d39c8282c837b4f1b81c95a7c62440f814b7
Oliver <1@2.de>
2018-11-29 04:54:37 +0100

    move movie data init to init method

--- src/main/java/net/kleditzsch/SmartHome/app/movie/MovieApplication.java
+++ src/main/java/net/kleditzsch/SmartHome/app/movie/MovieApplication.java
@@ -45,74 +45,6 @@ public class MovieApplication implements SubApplication {
      */
     public void init() {
 
-
-    }
-
-    /**
-     * initalisiert die Webinhalte der Anwendung
-     *
-     * @param contextHandler Context Handler
-     */
-    public void initWebContext(ServletContextHandler contextHandler) {
-
-        contextHandler.addServlet(MovieIndexServlet.class, "/movie/");
-        contextHandler.addServlet(MovieIndexServlet.class, "/movie/index");
-        contextHandler.addServlet(MovieNewMovieListServlet.class, "/movie/newmovies");
-        contextHandler.addServlet(MovieViewSoonMovieListServlet.class, "/movie/viewsonnmovies");
-        contextHandler.addServlet(MovieFskImageServlet.class, "/movie/fsklogo");
-        contextHandler.addServlet(MovieCoverImageServlet.class, "/movie/cover");
-        contextHandler.addServlet(MovieMovieListServlet.class, "/movie/movie");
-        contextHandler.addServlet(MovieMovieFormServlet.class, "/movie/movieform");
-        contextHandler.addServlet(MovieMovieViewServlet.class, "/movie/movieview");
-        contextHandler.addServlet(MovieMovieDeleteServlet.class, "/movie/moviedelete");
-        contextHandler.addServlet(MovieUpdateRatingServlet.class, "/movie/updaterating");
-        contextHandler.addServlet(MovieUpdateViewSoonServlet.class, "/movie/updateviewsoon");
-        contextHandler.addServlet(MovieMovieBoxListServlet.class, "/movie/moviebox");
-        contextHandler.addServlet(MovieMovieBoxFormServlet.class, "/movie/movieboxform");
-        contextHandler.addServlet(MovieBoxMovieFormServlet.class, "/movie/boxmovieform");
-        contextHandler.addServlet(MovieMovieBoxViewServlet.class, "/movie/movieboxview");
-        contextHandler.addServlet(MovieBoxMovieOrderServlet.class, "/movie/boxmovieorder");
-        contextHandler.addServlet(MovieBoxMovieDeleteServlet.class, "/movie/boxmoviedelete");
-        contextHandler.addServlet(MovieMovieBoxDeleteServlet.class, "/movie/movieboxdelete");
-        contextHandler.addServlet(MovieTmdbSearchServlet.class, "/movie/tmdbsearch");
-        contextHandler.addServlet(MovieTmdbMovieInfoServlet.class, "/movie/tmdbmovieinfo");
-        contextHandler.addServlet(MovieMovieSeriesListServlet.class, "/movie/movieseries");
-        contextHandler.addServlet(MovieMovieSeriesFormServlet.class, "/movie/movieseriesform");
-        contextHandler.addServlet(MovieMovieSeriesViewServlet.class, "/movie/movieseriesview");
-        contextHandler.addServlet(MovieSeriesMovieFormServlet.class, "/movie/seriesmovieform");
-        contextHandler.addServlet(MovieSeriesMovieOrderServlet.class, "/movie/seriesmovieorder");
-        contextHandler.addServlet(MovieSeriesMovieDeleteServlet.class, "/movie/seriesmoviedelete");
-        contextHandler.addServlet(MovieMovieSeriesDeleteServlet.class, "/movie/movieseriesdelete");
-        contextHandler.addServlet(MovieSeriesMovieSearchServlet.class, "/movie/seriesmoviesearch");
-        contextHandler.addServlet(MovieSearchServlet.class, "/movie/search");
-        contextHandler.addServlet(MovieSearchAllServlet.class, "/movie/searchall");
-        contextHandler.addServlet(MovieSearchPersonMoviesServlet.class, "/movie/serachpersonmovies");
-
-        contextHandler.addServlet(MovieAdminIndexServlet.class, "/movie/admin/");
-        contextHandler.addServlet(MovieAdminIndexServlet.class, "/movie/admin/index");
-        contextHandler.addServlet(MoviePersonListServlet.class, "/movie/admin/person");
-        contextHandler.addServlet(MoviePersonFormServlet.class, "/movie/admin/personform");
-        contextHandler.addServlet(MoviePersonDeleteServlet.class, "/movie/admin/persondelete");
-        contextHandler.addServlet(MovieDiscListServlet.class, "/movie/admin/disc");
-        contextHandler.addServlet(MovieDiscFormServlet.class, "/movie/admin/discform");
-        contextHandler.addServlet(MovieDiscDeleteServlet.class, "/movie/admin/discdelete");
-        contextHandler.addServlet(MovieDiscOrderServlet.class, "/movie/admin/discorder");
-        contextHandler.addServlet(MovieGenreListServlet.class, "/movie/admin/genre");
-        contextHandler.addServlet(MovieGenreFormServlet.class, "/movie/admin/genreform");
-        contextHandler.addServlet(MovieGenreDeleteServlet.class, "/movie/admin/genredelete");
-        contextHandler.addServlet(MovieFskListServlet.class, "/movie/admin/fsk");
-        contextHandler.addServlet(MovieFskFormServlet.class, "/movie/admin/fskform");
-        contextHandler.addServlet(MovieFskDeleteServlet.class, "/movie/admin/fskdelete");
-        contextHandler.addServlet(MovieFskOrderServlet.class, "/movie/admin/fskorder");
-        contextHandler.addServlet(MovieStatisticServlet.class, "/movie/admin/statistic");
-        contextHandler.addServlet(MovieSettingsServlet.class, "/movie/admin/settings");
-    }
-
-    /**
-     * startet die Anwendung
-     */
-    public void start() {
-
         //Medien initalisieren
         DiscEditor discEditor = DiscEditor.createAndLoad();
         if (discEditor.getData().size() == 0) {
@@ -246,6 +178,74 @@ public class MovieApplication implements SubApplication {
             MongoCollection collection = Application.getInstance().getDatabaseCollection(MovieSeriesEditor.COLLECTION);
             collection.createIndex(Indexes.text("search"), new IndexOptions().defaultLanguage("german"));
         }
+    }
+
+    /**
+     * initalisiert die Webinhalte der Anwendung
+     *
+     * @param contextHandler Context Handler
+     */
+    public void initWebContext(ServletContextHandler contextHandler) {
+
+        contextHandler.addServlet(MovieIndexServlet.class, "/movie/");
+        contextHandler.addServlet(MovieIndexServlet.class, "/movie/index");
+        contextHandler.addServlet(MovieNewMovieListServlet.class, "/movie/newmovies");
+        contextHandler.addServlet(MovieViewSoonMovieListServlet.class, "/movie/viewsonnmovies");
+        contextHandler.addServlet(MovieFskImageServlet.class, "/movie/fsklogo");
+        contextHandler.addServlet(MovieCoverImageServlet.class, "/movie/cover");
+        contextHandler.addServlet(MovieMovieListServlet.class, "/movie/movie");
+        contextHandler.addServlet(MovieMovieFormServlet.class, "/movie/movieform");
+        contextHandler.addServlet(MovieMovieViewServlet.class, "/movie/movieview");
+        contextHandler.addServlet(MovieMovieDeleteServlet.class, "/movie/moviedelete");
+        contextHandler.addServlet(MovieUpdateRatingServlet.class, "/movie/updaterating");
+        contextHandler.addServlet(MovieUpdateViewSoonServlet.class, "/movie/updateviewsoon");
+        contextHandler.addServlet(MovieMovieBoxListServlet.class, "/movie/moviebox");
+        contextHandler.addServlet(MovieMovieBoxFormServlet.class, "/movie/movieboxform");
+        contextHandler.addServlet(MovieBoxMovieFormServlet.class, "/movie/boxmovieform");
+        contextHandler.addServlet(MovieMovieBoxViewServlet.class, "/movie/movieboxview");
+        contextHandler.addServlet(MovieBoxMovieOrderServlet.class, "/movie/boxmovieorder");
+        contextHandler.addServlet(MovieBoxMovieDeleteServlet.class, "/movie/boxmoviedelete");
+        contextHandler.addServlet(MovieMovieBoxDeleteServlet.class, "/movie/movieboxdelete");
+        contextHandler.addServlet(MovieTmdbSearchServlet.class, "/movie/tmdbsearch");
+        contextHandler.addServlet(MovieTmdbMovieInfoServlet.class, "/movie/tmdbmovieinfo");
+        contextHandler.addServlet(MovieMovieSeriesListServlet.class, "/movie/movieseries");
+        contextHandler.addServlet(MovieMovieSeriesFormServlet.class, "/movie/movieseriesform");
+        contextHandler.addServlet(MovieMovieSeriesViewServlet.class, "/movie/movieseriesview");
+        contextHandler.addServlet(MovieSeriesMovieFormServlet.class, "/movie/seriesmovieform");
+        contextHandler.addServlet(MovieSeriesMovieOrderServlet.class, "/movie/seriesmovieorder");
+        contextHandler.addServlet(MovieSeriesMovieDeleteServlet.class, "/movie/seriesmoviedelete");
+        contextHandler.addServlet(MovieMovieSeriesDeleteServlet.class, "/movie/movieseriesdelete");
+        contextHandler.addServlet(MovieSeriesMovieSearchServlet.class, "/movie/seriesmoviesearch");
+        contextHandler.addServlet(MovieSearchServlet.class, "/movie/search");
+        contextHandler.addServlet(MovieSearchAllServlet.class, "/movie/searchall");
+        contextHandler.addServlet(MovieSearchPersonMoviesServlet.class, "/movie/serachpersonmovies");
+
+        contextHandler.addServlet(MovieAdminIndexServlet.class, "/movie/admin/");
+        contextHandler.addServlet(MovieAdminIndexServlet.class, "/movie/admin/index");
+        contextHandler.addServlet(MoviePersonListServlet.class, "/movie/admin/person");
+        contextHandler.addServlet(MoviePersonFormServlet.class, "/movie/admin/personform");
+        contextHandler.addServlet(MoviePersonDeleteServlet.class, "/movie/admin/persondelete");
+        contextHandler.addServlet(MovieDiscListServlet.class, "/movie/admin/disc");
+        contextHandler.addServlet(MovieDiscFormServlet.class, "/movie/admin/discform");
+        contextHandler.addServlet(MovieDiscDeleteServlet.class, "/movie/admin/discdelete");
+        contextHandler.addServlet(MovieDiscOrderServlet.class, "/movie/admin/discorder");
+        contextHandler.addServlet(MovieGenreListServlet.class, "/movie/admin/genre");
+        contextHandler.addServlet(MovieGenreFormServlet.class, "/movie/admin/genreform");
+        contextHandler.addServlet(MovieGenreDeleteServlet.class, "/movie/admin/genredelete");
+        contextHandler.addServlet(MovieFskListServlet.class, "/movie/admin/fsk");
+        contextHandler.addServlet(MovieFskFormServlet.class, "/movie/admin/fskform");
+        contextHandler.addServlet(MovieFskDeleteServlet.class, "/movie/admin/fskdelete");
+        contextHandler.addServlet(MovieFskOrderServlet.class, "/movie/admin/fskorder");
+        contextHandler.addServlet(MovieStatisticServlet.class, "/movie/admin/statistic");
+        contextHandler.addServlet(MovieSettingsServlet.class, "/movie/admin/settings");
+    }
+
+    /**
+     * startet die Anwendung
+     */
+    public void start() {
+
+
     }
 
     /**
